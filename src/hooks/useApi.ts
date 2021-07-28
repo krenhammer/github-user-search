a0faf467d827d22d232f769921abf2cc370f9896
Skip <lkrenek@gmail.com>
2021-07-28 13:54:49 -0400

    Refresh Query on Filter Change

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import { useAPIClient, useInitAPIClient } from '../store/apiClientStore';
 import store from '../store/store'
 import { Endpoints, OctokitResponse } from '@octokit/types';
+import { useSnapshot } from 'valtio';
 
 export type UsersResponse = OctokitResponse<Endpoints["GET /search/users"]["response"]["data"]>;
 
@@ -11,8 +12,10 @@ export const useApi = () => {
     useInitAPIClient();
 
     const client = useAPIClient();
+
+    const snap = useSnapshot(store);
     
-    const queriedData = useQuery<UsersResponse, string>('PLCalcData', async () => {
+    const queriedData = useQuery<UsersResponse, string>(snap.userFilter, async () => {
         const users = await client?.search.users({
             q: store.userFilter
         }) as UsersResponse;
