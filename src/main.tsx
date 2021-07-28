0160764ca8482b2c9c65f19a953e77974bde0c95
Skip <lkrenek@gmail.com>
2021-07-28 14:11:50 -0400

    Add Initial Route Structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Route, HashRouter as Router, Switch } from 'react-router-dom';
 import './index.css'
-import App from './App'
+// import App from './App'
+import { Users, UserDetail } from './pages';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/search/:query?">
+            <Users />
+          </Route>
+          <Route path="/user/:username?">
+            <UserDetail />
+          </Route>
+        </Switch>
+    </Router>
+  
   </React.StrictMode>,
   document.getElementById('root')
 )
---
+++ src/pages/UserDetail.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const UserDetail: React.FC<any> = () => {
+    return (
+        <></>
+    );
+}
